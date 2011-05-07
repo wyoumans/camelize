16b0482d2f9933ec41f4d39c93474f474c321e88
William Youmans <will@williamyoumans.com>
2011-05-07 01:29:20 -0600

    inflector

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+echo<<<HTML
+
+<html> 
+  <head> 
+    <title>William Youmans | Web Developer, World Traveler</title> 
+    <meta charset="utf-8" /> 
+    <meta name="keywords" content="William Youmans Salt Lake City Utah Web Development Design CSS HTML PHP freelance photoshop inkscape gimp illustrator cheap free reasonable solution ninja crane klanoma web services"> 
+    <meta name="google-site-verification" content="Muk7w0OzK25unpvJS-Zu6SVz9d6nMMmvDeDm7aVeeCI" /> 
+    <meta name="description" content="William Youmans"> 
+    <meta name="author" content="William Youmans is a freelance web developer living, working, and loving Salt Lake City, Utah."> 
+    <meta name="ROBOTS" content="ALL">
+
+    <!-- Javascript --> 
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+ 
+    <!-- Stylesheets --> 
+    <link rel="stylesheet" type="text/css" href="assets/reset.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="assets/style.css" media="screen" />
+    
+    <!--Favicon --> 
+    <link rel="shortcut icon" href="favicon.ico"> 
+    
+  </head> 
+  <body>
+    <div id="content">
+      <form>
+        <input type="text" value="" id="camelize"
+      </form>
+    </div>
+  </body>
+</html>
+
+<script type="text/javascript">
+  $(function(){
+    
+  
+  });
+  
+  function camelize(){
+    
+  }
+</script>
+
+HTML;
+
+?>
