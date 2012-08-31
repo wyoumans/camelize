6b1eb77de6bc0000f9248341c9c698317f3429ed
William Youmans <will@williamyoumans.com>
2012-08-31 11:47:27 -0600

    added support for copy paste with mouse

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $host = 'camelize';
 if(isset($_SERVER['HTTP_HOST'])){
   $host = $_SERVER['HTTP_HOST'];
 }
-
+date_default_timezone_set('America/Denver');
 $year = date("Y");
 
 ?>
@@ -62,6 +62,9 @@ $year = date("Y");
         $("#camelize").keyup(function(){
           $("#result").html(camelize($(this).val()));
         });
+        $("#camelize").bind("input propertychange", function() {
+           console.log($(this).val());
+        });
       });
 
       function camelize( s ) {
@@ -71,22 +74,22 @@ $year = date("Y");
       }
     </script>
 
-<?php if(!preg_match("/local/", $host)): ?>
-    <script type="text/javascript">
+    <?php if(!preg_match("/local/", $host) && !preg_match("/lan/", $host)): ?>
+      <script type="text/javascript">
 
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-11130262-12']);
-      _gaq.push(['_trackPageview']);
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-11130262-12']);
+        _gaq.push(['_trackPageview']);
 
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
 
-    </script>
+      </script>
 
-<?php endif ?>
+    <?php endif ?>
 
   </body>
 </html>
