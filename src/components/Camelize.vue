f7c7de4fb197ce94e0d1731b957e309bf2dd5f19
Will <will@williamyoumans.com>
2019-02-16 23:27:51 -0500

    styling with tailwind

--- src/components/Camelize.vue
+++ src/components/Camelize.vue
@@ -1,14 +1,31 @@
 <template>
-  <div>
-    <h1>Camelize</h1>
+  <div class="flex flex-col justify-center items-center content-center p-8 h-screen">
+    <div class="w-full md:w-3/4 lg:w-2/3 flex flex-col">
 
-    <input
-      type="text"
-      v-model.trim="message"
-      placeholder="Begin typing..."
-    >
+      <h1 class="shadow font-light text-blue-darker text-right -mb-1 pr-2">Camelize</h1>
 
-    <p>{{ camelizedMessage }}</p>
+      <div class="bg-blue-darker shadow-lg rounded-lg p-2">
+        <input
+          type="text"
+          v-model.trim="message"
+          placeholder="Begin typing..."
+          class="appearance-none rounded block text-lg w-full py-2 px-3 text-blue-dark leading-tight focus:outline-none focus:shadow-outline"
+        >
+        <div class="rounded p-20 my-2 bg-blue-dark text-3xl text-blue-lighter font-mono text-center leading-loose" style="word-break: break-word;">
+          {{ camelizedMessage }}
+        </div>
+
+        <div class="flex flex-col md:flex-row md:justify-between px-2 text-center md:text-left leading-loose md:leading-normal">
+          <small class="font-light text-white">
+            &copy;{{ year }} <a href="https://www.williamyoumans.com" target="_blank" class="text-blue-light hover:text-blue-lighter no-underline">William Youmans</a>
+          </small>
+
+          <small class="font-light">
+            <a href="http://en.wikipedia.org/wiki/CamelCase" targe="_blank" class="text-blue-light hover:text-blue-lighter no-underline">What is this?</a>
+          </small>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,7 +35,8 @@ export default {
 
   data() {
     return {
-      message: ''
+      message: '',
+      year: 2019
     };
   },
 
