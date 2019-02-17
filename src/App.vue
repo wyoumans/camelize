bc9576fe7f30004cb6b0fbb1734926a544b02d4a
Will <will@williamyoumans.com>
2019-02-16 22:06:37 -0500

    basic camelize component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Camelize/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Camelize from './components/Camelize.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Camelize
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Camelize.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Camelize</h1>
+
+    <input
+      type="text"
+      v-model.trim="message"
+      placeholder="Begin typing..."
+    >
+
+    <p>{{ camelizedMessage }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Camelize',
+
+  data() {
+    return {
+      message: ''
+    };
+  },
+
+  computed: {
+    camelizedMessage() {
+      return this.message.split(' ').join('_');
+    }
+  }
+}
+</script>
