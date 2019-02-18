65d09e9a15961c0fbb8eeceb48ae9a7907ebebd9
Will <will@williamyoumans.com>
2019-02-17 21:25:53 -0500

    include responsive styles in purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 const purgecss = require("@fullhuman/postcss-purgecss");
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
 module.exports = {
   plugins: [
     tailwindcss("./tailwind.js"),
@@ -8,7 +15,11 @@ module.exports = {
       content: [
         "./public/**/*.html",
         "./src/**/*.vue"
-      ]
+      ],
+      extractors: [{
+        extractor: TailwindExtractor,
+        extensions: ['vue', 'html']
+      }]
     }),
     autoprefixer({
       add: true,
