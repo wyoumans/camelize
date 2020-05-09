cf8f0e7223fd48bd46d506e5a43af2052b71a891
Will <will@williamyoumans.com>
2020-05-08 22:14:55 -0400

    upgraded vue and tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,7 @@
 module.exports = {
-  plugins: [
-    require("tailwindcss")("./tailwind.js"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production"
-      ? [
-          require("@fullhuman/postcss-purgecss")({
-            content: ["./src/**/*.vue", "./public/**/*.html"],
-            extractors: [
-              {
-                extractor: class {
-                  static extract(content) {
-                    return content.match(/[a-zA-Z0-9-:_/]+/g) || [];
-                  }
-                },
-                extensions: ["vue", "html"],
-              },
-            ],
-          }),
-        ]
-      : []),
-  ],
-};
+  plugins: {
+    tailwindcss: {},
+    'vue-cli-plugin-tailwind/purgecss': {},
+    autoprefixer: {}
+  }
+}
--- src/components/Camelize.vue
+++ src/components/Camelize.vue
@@ -1,44 +1,53 @@
 <template>
-  <div class="flex flex-col justify-center items-center content-center p-8 min-h-screen">
+  <div
+    class="flex flex-col justify-center items-center content-center p-8 min-h-screen"
+  >
     <div class="w-full md:w-3/4 lg:w-2/3 flex flex-col">
-      <h1 class="font-light text-blue-darker text-right -mb-1 pr-2">Camelize</h1>
+      <h1 class="text-3xl font-400 text-blue-900 text-right -mb-1 pr-2">
+        Camelize
+      </h1>
 
-      <div class="bg-blue-darker shadow-lg rounded-lg p-2">
+      <div class="bg-blue-900 shadow-lg rounded-lg p-2">
         <input
           type="text"
           v-model.trim="message"
           placeholder="Begin typing..."
-          class="appearance-none rounded block text-lg w-full py-2 px-3 text-blue-dark leading-tight focus:outline-none focus:shadow-outline"
+          class="appearance-none rounded block text-lg w-full py-2 px-3 text-blue-600 leading-tight focus:outline-none focus:shadow-outline"
         />
         <div
-          class="rounded p-20 my-2 bg-blue-dark text-3xl text-blue-lighter font-mono text-center leading-loose"
+          class="rounded p-20 my-2 bg-blue-600 text-3xl text-blue-200 font-mono text-center leading-loose"
           style="min-height: 220px; word-break: break-word;"
-        >{{ camelizedMessage }}</div>
+        >
+          {{ camelizedMessage }}
+        </div>
 
         <div
           class="flex flex-col md:flex-row md:justify-between px-2 text-center md:text-left leading-loose md:leading-normal"
         >
-          <small class="font-light text-white">
+          <small class="font-400 text-white">
             &copy;{{ year }}
             <a
               href="https://www.forefrontsoftware.com"
               target="_blank"
-              class="text-blue-light hover:text-blue-lighter no-underline"
-            >Forefront Software</a>
+              class="text-blue-400 hover:text-blue-200 no-underline"
+              >Forefront Software</a
+            >
           </small>
-          <small class="font-light text-white">
+          <small class="font-400 text-white">
             <a
               href="https://www.ctosumo.com"
               target="_blank"
-              class="text-blue-light hover:text-blue-lighter no-underline"
-            >Need Tech Consulting? Try CTO Sumo!</a>
+              class="text-blue-400 hover:text-blue-200 no-underline"
+              >Need Tech Consulting? Try CTO Sumo!</a
+            >
           </small>
-          <small class="font-light">
+          <small class="font-400">
             <a
               href="http://en.wikipedia.org/wiki/CamelCase"
               target="_blank"
-              class="text-blue-light hover:text-blue-lighter no-underline"
-            >What is this?</a>
+              class="text-blue-400 hover:text-blue-200 no-underline"
+              >What is this?</a
+            >
           </small>
         </div>
       </div>
@@ -53,7 +62,7 @@ export default {
   data() {
     return {
       message: "",
-      year: 2019
+      year: 2019,
     };
   },
 
@@ -64,7 +73,7 @@ export default {
         if (p2) return p2.toUpperCase();
         return p1.toLowerCase();
       });
-    }
-  }
+    },
+  },
 };
 </script>
