1f70272d94e10279fb6b10a7712b9c87cc64abd4
Will <will@williamyoumans.com>
2020-12-23 20:57:49 -0500

    calculate the current year

--- src/components/Camelize.vue
+++ src/components/Camelize.vue
@@ -1,43 +1,43 @@
 <template>
   <div
-    class="flex flex-col justify-center items-center content-center p-8 min-h-screen"
+    class="flex flex-col items-center content-center justify-center min-h-screen p-8"
   >
-    <div class="w-full md:w-3/4 lg:w-2/3 flex flex-col">
-      <h1 class="text-3xl font-400 text-blue-900 text-right -mb-1 pr-2">
+    <div class="flex flex-col w-full md:w-3/4 lg:w-2/3">
+      <h1 class="pr-2 -mb-1 text-3xl text-right text-blue-900 font-400">
         Camelize
       </h1>
 
-      <div class="bg-blue-900 shadow-lg rounded-lg p-2">
+      <div class="p-2 bg-blue-900 rounded-lg shadow-lg">
         <input
           type="text"
           v-model.trim="message"
           placeholder="Begin typing..."
-          class="appearance-none rounded block text-lg w-full py-2 px-3 text-blue-600 leading-tight focus:outline-none focus:shadow-outline"
+          class="block w-full px-3 py-2 text-lg leading-tight text-blue-600 rounded appearance-none focus:outline-none focus:shadow-outline"
         />
         <div
-          class="rounded p-20 my-2 bg-blue-600 text-3xl text-blue-200 font-mono text-center leading-loose"
+          class="p-20 my-2 font-mono text-3xl leading-loose text-center text-blue-200 bg-blue-600 rounded"
           style="min-height: 220px; word-break: break-word;"
         >
           {{ camelizedMessage }}
         </div>
 
         <div
-          class="flex flex-col md:flex-row md:justify-between px-2 text-center md:text-left leading-loose md:leading-normal"
+          class="flex flex-col px-2 leading-loose text-center md:flex-row md:justify-between md:text-left md:leading-normal"
         >
-          <small class="font-400 text-white">
+          <small class="text-white font-400">
             &copy;{{ year }}
             <a
               href="https://www.forefrontsoftware.com"
               target="_blank"
-              class="text-blue-400 hover:text-blue-200 no-underline"
+              class="text-blue-400 no-underline hover:text-blue-200"
               >Forefront Software</a
             >
           </small>
-          <small class="font-400 text-white">
+          <small class="text-white font-400">
             <a
               href="https://www.ctosumo.com"
               target="_blank"
-              class="text-blue-400 hover:text-blue-200 no-underline"
+              class="text-blue-400 no-underline hover:text-blue-200"
               >Need Tech Consulting? Try CTO Sumo!</a
             >
           </small>
@@ -45,7 +45,7 @@
             <a
               href="http://en.wikipedia.org/wiki/CamelCase"
               target="_blank"
-              class="text-blue-400 hover:text-blue-200 no-underline"
+              class="text-blue-400 no-underline hover:text-blue-200"
               >What is this?</a
             >
           </small>
@@ -61,8 +61,7 @@ export default {
 
   data() {
     return {
-      message: "",
-      year: 2019,
+      message: ""
     };
   },
 
@@ -74,6 +73,11 @@ export default {
         return p1.toLowerCase();
       });
     },
+
+    year() {
+      let d = new Date();
+      return d.getFullYear();
+    }
   },
 };
 </script>
