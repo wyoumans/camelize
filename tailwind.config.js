75f42e96fd7b96b89946c29728235f04723f2ccc
Will <will@williamyoumans.com>
2020-12-30 20:52:28 -0500

    Upgraded to vue 3 with vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  purge: ["./src/**/*.vue", "./public/**/*.html"],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [],
-};
+}
